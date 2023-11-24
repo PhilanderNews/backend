7de6b28e67753a07b2a0b3d71692e68945b1af43
Befous <befousmain@gmail.com>
2023-11-24 07:44:10 +0700

    Update : hapus struct ga penting, tambah yang penting

--- type.go
+++ type.go
@@ -1,9 +1,5 @@
 package peda
 
-type Properties struct {
-	Name string `json:"name" bson:"name"`
-}
-
 type User struct {
 	Name     string    `json:"name,omitempty" bson:"name,omitempty"`
 	Email    string    `json:"email,omitempty" bson:"email,omitempty"`
@@ -18,47 +14,36 @@ type SemuaRole struct {
 	User   bool `json:"user" bson:"user"`
 }
 
-type AuthorizationStruct struct {
-	Status  bool     `json:"status" bson:"status"`
-	Data    UserAuth `json:"data,omitempty" bson:"data,omitempty"`
-	Message string   `json:"message,omitempty" bson:"message,omitempty"`
-	Token   string   `json:"token,omitempty" bson:"token,omitempty"`
-}
-
-type UserAuth struct {
-	Name     string    `json:"name,omitempty" bson:"name,omitempty"`
-	Email    string    `json:"email,omitempty" bson:"email,omitempty"`
-	Username string    `json:"username,omitempty" bson:"username,omitempty"`
-	Role     SemuaRole `json:"role,omitempty" bson:"role,omitempty"`
-}
-
-type Credential struct {
+type CredentialUser struct {
 	Status  bool   `json:"status" bson:"status"`
+	Data    User   `json:"data,omitempty" bson:"data,omitempty"`
+	Message string `json:"message,omitempty" bson:"message,omitempty"`
 	Token   string `json:"token,omitempty" bson:"token,omitempty"`
-	Message string `json:"message,omitempty" bson:"message,omitempty"`
-	Name    string `json:"name,omitemptye" bson:"name,omitempty"`
-	Role    string `json:"role,omitempty" bson:"role,omitempty"`
 }
-
-type ResponseDataUser struct {
+type CredentialBerita struct {
 	Status  bool   `json:"status" bson:"status"`
+	Data    Berita `json:"data,omitempty" bson:"data,omitempty"`
 	Message string `json:"message,omitempty" bson:"message,omitempty"`
-	Data    []User `json:"data,omitempty" bson:"data,omitempty"`
-}
-
-type Response struct {
-	Token string `json:"token,omitempty" bson:"token,omitempty"`
-}
-
-type Jaja struct {
-	Token string      `json:"token" bson:"token"`
-	Data  interface{} `json:"data" bson:"data"`
+	Token   string `json:"token,omitempty" bson:"token,omitempty"`
 }
 
 type Berita struct {
-	ID       string `json:"id" bson:"id"`
-	Kategori string `json:"kategori" bson:"kategori"`
-	Judul    string `json:"judul" bson:"judul"`
-	Preview  string `json:"preview" bson:"preview"`
-	Konten   string `json:"konten" bson:"konten"`
+	ID       string   `json:"id" bson:"id"`
+	Kategori string   `json:"kategori" bson:"kategori"`
+	Judul    string   `json:"judul" bson:"judul"`
+	Preview  string   `json:"preview" bson:"preview"`
+	Konten   Paragraf `json:"konten" bson:"konten"`
+}
+
+type Paragraf struct {
+	Paragraf1  string `json:"paragraf1,omitempty" bson:"paragraf1,omitempty"`
+	Paragraf2  string `json:"paragraf2,omitempty" bson:"paragraf2,omitempty"`
+	Paragraf3  string `json:"paragraf3,omitempty" bson:"paragraf3,omitempty"`
+	Paragraf4  string `json:"paragraf4,omitempty" bson:"paragraf4,omitempty"`
+	Paragraf5  string `json:"paragraf5,omitempty" bson:"paragraf5,omitempty"`
+	Paragraf6  string `json:"paragraf6,omitempty" bson:"paragraf6,omitempty"`
+	Paragraf7  string `json:"paragraf7,omitempty" bson:"paragraf7,omitempty"`
+	Paragraf8  string `json:"paragraf8,omitempty" bson:"paragraf8,omitempty"`
+	Paragraf9  string `json:"paragraf9,omitempty" bson:"paragraf9,omitempty"`
+	Paragraf10 string `json:"paragraf10,omitempty" bson:"paragraf10,omitempty"`
 }
