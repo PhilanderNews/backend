5d8328bb87db6ef05ef567b6971d74dafca77dfb
Befous <befousmain@gmail.com>
2023-11-24 07:43:42 +0700

    Update : hapus function ga penting, tambah funtion yang kepakek

--- peda_test.go
+++ peda_test.go
@@ -4,136 +4,28 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/aiteung/atdb"
 	"github.com/whatsauth/watoken"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
-func TestUpdateGetData(t *testing.T) {
-	mconn := SetConnection("mongoenv", "unistore")
-	datagedung := GetAllUser(mconn, "user")
-	fmt.Println(datagedung)
-}
-
-// 	result := GCFCreateHandler(MONGOCONNSTRINGENV, dbname, collectionname, datauser)
-// 	fmt.Println(result)
-// 	// You can add assertions here to validate the result, or check the database for the created user.
-// }
-
-func TestCreateNewUserRole(t *testing.T) {
-	var userdata User
-	userdata.Username = "unistore"
-	userdata.Password = "unistore"
-	mconn := SetConnection("MONGOCONNSTRINGENV", "unistore")
-	CreateNewUserRole(mconn, "user", userdata)
-}
-
-func TestDeleteUser(t *testing.T) {
-
-	mconn := SetConnection("mongoenv", "unistore")
-	var userdata User
-	userdata.Username = "unistore"
-	DeleteUser(mconn, "user", userdata)
-}
-
-func TestGFCPostHandlerUser(t *testing.T) {
-	mconn := SetConnection("mongoenv", "unistore")
-	var userdata User
-	userdata.Username = "unistore"
-	userdata.Password = "unistore"
-	CreateNewUserRole(mconn, "user", userdata)
-}
-
-func TestFunciionUser(t *testing.T) {
-	mconn := SetConnection("mongoenv", "unistore")
-	var userdata User
-	userdata.Username = "unistore"
-	userdata.Password = "unistore"
-	CreateNewUserRole(mconn, "user", userdata)
-}
-
-func TestGeneratePasswordHashh(t *testing.T) {
-	password := "secret"
-	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
-
-	fmt.Println("Password:", password)
-	fmt.Println("Hash:    ", hash)
-
-	match := CheckPasswordHash(password, hash)
-	fmt.Println("Match:   ", match)
-}
-func TestHashFunctionn(t *testing.T) {
-	mconn := SetConnection("mongoenv", "unistore")
-	var userdata User
-	userdata.Username = "unistore"
-	userdata.Password = "unistore"
-
-	filter := bson.M{"username": userdata.Username}
-	res := atdb.GetOneDoc[User](mconn, "user", filter)
-	fmt.Println("Mongo User Result: ", res)
-	hash, _ := HashPassword(userdata.Password)
-	fmt.Println("Hash Password : ", hash)
-	match := CheckPasswordHash(userdata.Password, res.Password)
-	fmt.Println("Match:   ", match)
-
-}
-func TestFindUser(t *testing.T) {
-	var userdata User
-	userdata.Username = "unistore"
-	mconn := SetConnection("mongoenv", "unistore")
-	res := FindUser(mconn, "user", userdata)
-	fmt.Println(res)
-}
-
-func TestGeneratePasswordHash(t *testing.T) {
-	password := "unistore"
-	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
-
-	fmt.Println("Password:", password)
-	fmt.Println("Hash:    ", hash)
-	match := CheckPasswordHash(password, hash)
-	fmt.Println("Match:   ", match)
-}
-func TestGeneratePrivateKeyPaseto(t *testing.T) {
+func TestGeneratePaseto(t *testing.T) {
 	privateKey, publicKey := watoken.GenerateKey()
-	fmt.Println(privateKey)
-	fmt.Println(publicKey)
-	hasil, err := watoken.Encode("unistore", privateKey)
-	fmt.Println(hasil, err)
+	fmt.Println("privateKey" + privateKey)
+	fmt.Println("publicKey" + publicKey)
 }
 
-func TestHashFunction(t *testing.T) {
-	mconn := SetConnection("mongoenv", "unistore")
-	var userdata User
-	userdata.Username = "unistore"
-	userdata.Password = "unistore"
+func TestEncode(t *testing.T) {
+	privateKey := "8c92e028bf9dc2ad8d7244a08b611220845a7f43fb7986a6af805cbda811b96c02a51c4853c18c4a4bbedd29661618e7bec9041d4063caec8cc84c601c20281b"
+	userid := "mubarok"
 
-	filter := bson.M{"username": userdata.Username}
-	res := atdb.GetOneDoc[User](mconn, "user", filter)
-	fmt.Println("Mongo User Result: ", res)
-	hash, _ := HashPassword(userdata.Password)
-	fmt.Println("Hash Password : ", hash)
-	match := CheckPasswordHash(userdata.Password, res.Password)
-	fmt.Println("Match:   ", match)
-
+	tokenstring, err := watoken.Encode(userid, privateKey)
+	fmt.Println("error : ", err)
+	fmt.Println("token : ", tokenstring)
 }
 
-func TestIsPasswordValid(t *testing.T) {
-	mconn := SetConnection("mongoenv", "unistore")
-	var userdata User
-	userdata.Username = "unistore"
-	userdata.Password = "unistore"
+func TestDecode(t *testing.T) {
+	publicKey := "02a51c4853c18c4a4bbedd29661618e7bec9041d4063caec8cc84c601c20281b"
 
-	anu := IsPasswordValid(mconn, "user", userdata)
-	fmt.Println(anu)
-}
-
-func TestRole(t *testing.T) {
-	role := "admin"
-	if role == "admin" {
-		fmt.Println("anu")
-	} else {
-		fmt.Println("2sad")
-	}
-
+	tokenstring := "v4.public.eyJleHAiOiIyMDIzLTExLTIzVDExOjA5OjQ0KzA3OjAwIiwiaWF0IjoiMjAyMy0xMS0yM1QwOTowOTo0NCswNzowMCIsImlkIjoiaWJyb2hpbSIsIm5iZiI6IjIwMjMtMTEtMjNUMDk6MDk6NDQrMDc6MDAifRrPRTXnMvVDjYN_Eb27_GYyovqHZCwI8ds5Rk7RxM2OyqiujCUzsTfZy1PlaAl7kv7wkQk9ST0oFJ3WD2Ih-Qg"
+	body := watoken.DecodeGetId(publicKey, tokenstring)
+	fmt.Println("isi : ", body)
 }
