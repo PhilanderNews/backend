c9cce0454276bcd8121b31b3398c36c89553c6e6
Befous <befousmain@gmail.com>
2023-11-28 23:19:37 +0700

    Update : test paseto

--- peda_test.go
+++ peda_test.go
@@ -3,29 +3,34 @@ package peda
 import (
 	"fmt"
 	"testing"
-
-	"github.com/whatsauth/watoken"
 )
 
+var privatekey2 = "085151de76c61415e40ee4440b3848d0317a28e58fc7f9ddff8455cca31fe499c78ba61336ef2a8ac88e3126e0c3ad0e4cdccc271a11240db3f61e91bb68a936"
+var publickey2 = "c78ba61336ef2a8ac88e3126e0c3ad0e4cdccc271a11240db3f61e91bb68a936"
+var encode = "v4.public.eyJleHAiOiIyMDIzLTExLTI5VDAwOjIyOjA1KzA3OjAwIiwiaWF0IjoiMjAyMy0xMS0yOFQyMjoyMjowNSswNzowMCIsIm5iZiI6IjIwMjMtMTEtMjhUMjI6MjI6MDUrMDc6MDAiLCJyb2xlIjoiYWRtaW4iLCJ1c2VybmFtZSI6Imlicm9oaW0ifXUeIuOthFir7tNFpkCsdb41IKBSDpYoAZlRhD3wgryojOf-e13zZQga_uxvrvv7aXdnX6PJWXWr4NJE_RXLyAE"
+
 func TestGeneratePaseto(t *testing.T) {
-	privateKey, publicKey := watoken.GenerateKey()
-	fmt.Println("privateKey" + privateKey)
-	fmt.Println("publicKey" + publicKey)
+	privateKey, publicKey := GenerateKey()
+	fmt.Println("privateKey: " + privateKey)
+	fmt.Println("publicKey: " + publicKey)
 }
 
 func TestEncode(t *testing.T) {
-	privateKey := "8c92e028bf9dc2ad8d7244a08b611220845a7f43fb7986a6af805cbda811b96c02a51c4853c18c4a4bbedd29661618e7bec9041d4063caec8cc84c601c20281b"
-	userid := "mubarok"
+	username := "ibrohim"
+	role := "admin"
 
-	tokenstring, err := watoken.Encode(userid, privateKey)
+	tokenstring, err := Encode(username, role, privatekey2)
 	fmt.Println("error : ", err)
 	fmt.Println("token : ", tokenstring)
 }
 
 func TestDecode(t *testing.T) {
-	publicKey := "02a51c4853c18c4a4bbedd29661618e7bec9041d4063caec8cc84c601c20281b"
+	pay, err := Decode(publickey2, encode)
+	user := DecodeGetUsername(publickey2, encode)
+	role := DecodeGetRole(publickey2, encode)
 
-	tokenstring := "v4.public.eyJleHAiOiIyMDIzLTExLTIzVDExOjA5OjQ0KzA3OjAwIiwiaWF0IjoiMjAyMy0xMS0yM1QwOTowOTo0NCswNzowMCIsImlkIjoiaWJyb2hpbSIsIm5iZiI6IjIwMjMtMTEtMjNUMDk6MDk6NDQrMDc6MDAifRrPRTXnMvVDjYN_Eb27_GYyovqHZCwI8ds5Rk7RxM2OyqiujCUzsTfZy1PlaAl7kv7wkQk9ST0oFJ3WD2Ih-Qg"
-	body := watoken.DecodeGetId(publicKey, tokenstring)
-	fmt.Println("isi : ", body)
+	fmt.Println("user :", user)
+	fmt.Println("role :", role)
+	fmt.Println("err : ", err)
+	fmt.Println("payload : ", pay)
 }
