3217aaefef31eb5f238a6096ed1450b2b880b7ae
Befous <befousmain@gmail.com>
2023-11-29 13:28:55 +0700

    Update : tambah struct buat komentar dan tambah payload name

--- type.go
+++ type.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Payload struct {
+	Name     string    `json:"name"`
 	Username string    `json:"username"`
 	Role     string    `json:"role"`
 	Exp      time.Time `json:"exp"`
@@ -52,4 +53,12 @@ type Paragraf struct {
 	Paragraf8  string `json:"paragraf8,omitempty" bson:"paragraf8,omitempty"`
 	Paragraf9  string `json:"paragraf9,omitempty" bson:"paragraf9,omitempty"`
 	Paragraf10 string `json:"paragraf10,omitempty" bson:"paragraf10,omitempty"`
+}
+
+type Komentar struct {
+	ID        string `json:"id" bson:"id"`
+	ID_berita string `json:"id_berita" bson:"id_berita"`
+	Name      string `json:"name,omitempty" bson:"name,omitempty"`
+	Tanggal   string `json:"tanggal" bson:"tanggal"`
+	Komentar  string `json:"komentar" bson:"komentar"`
 }
